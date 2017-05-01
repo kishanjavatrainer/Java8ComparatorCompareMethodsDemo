b1d76b217a4a627a6b4eaf8b7c7d2d6f12dc8693
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-05-01 15:59:54 +0530

    Java8ComparatorCompareMethodsDemo Source code

---
+++ Java8ComparatorCompareMethodsDemo/src/com/infotech/client/ClientTest.java
@@ -0,0 +1,33 @@
+package com.infotech.client;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.infotech.model.Employee;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		
+		List<Employee> empList = new ArrayList<>();
+		empList.add(new Employee("Davis", 30));
+		empList.add(new Employee("Alice", 23));
+		empList.add(new Employee("Barry", 34));
+		empList.add(new Employee("Martin", 35));
+		empList.add(new Employee("James", 45));
+		
+		empList.forEach(System.out::println);
+		System.out.println("-----------------------------------------------");
+		//Comparator<Employee> comp = Comparator.comparing(Employee::getName);
+		Comparator<Employee> comp = Comparator.comparing(Employee::getName, String.CASE_INSENSITIVE_ORDER);
+		Collections.sort(empList, comp);
+		
+		empList.forEach(System.out::println);
+		
+		
+		
+	}
+
+}
